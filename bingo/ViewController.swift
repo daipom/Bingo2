70034efd0b5fa3484010c7675d4f25ff18c203a5
kadowaki <kadowaki@amiya.co.jp>
2016-09-28 21:06:57 +0900

    Bingo_modified0928

--- bingo/ViewController.swift
+++ bingo/ViewController.swift
@@ -11,12 +11,15 @@ import Cocoa
 class ViewController: NSViewController {
     
     var bingoData : BingoDataEntity = BingoDataEntity()
-
+    var chanceValue = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.numField.hidden = true
+        //self.buttonFavorite.hideen = false
+        //画像を設定する
     }
 
     override var representedObject: AnyObject? {
@@ -31,9 +34,23 @@ class ViewController: NSViewController {
     
     @IBOutlet weak var numField: NSTextField!
     
+    @IBOutlet weak var chanceImage: NSImageView!
+    
     @IBAction func buttonMain(sender: AnyObject) {
-        labelMain.stringValue = bingoData.outputNextNum()
-        labelSub.stringValue = bingoData.outputPastNums()
+        chanceValue = (Int)(arc4random_uniform(5))
+        
+        if chanceValue == 0{
+            self.numField.hidden = false
+            //self.buttonFavorite.hidden = false
+            labelMain.stringValue = "Chance!"
+            //写真を出す
+            let myImage = NSImage(named: "testimage.png")
+            chanceImage.image = myImage
+        }
+        else{
+            labelMain.stringValue = bingoData.outputNextNum()
+            labelSub.stringValue = bingoData.outputPastNums()
+        }
     }
     
     @IBAction func buttonFavorite(sender: AnyObject) {
@@ -44,6 +61,9 @@ class ViewController: NSViewController {
                 labelSub.stringValue = bingoData.outputPastNums()
             }
         }
+        self.numField.hidden = false
+        //self.buttonFavorite.hidden = false
+        //写真を隠す
     }
 }
 
